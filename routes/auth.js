5959e53df958ffff414a22d73b4008d064bc0dee
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2021-04-21 21:45:44 +0530

    Made user model filename and imports lowercase due to heroku problems

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 const jwt = require("jsonwebtoken");
 const getUserById = async (req, res, next) => {
